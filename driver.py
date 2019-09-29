91842df2e4c55d37cdeef05b391b7049c15cd5c7
Wambugu "Innocent" Kironji <innocent.kironji@duke.edu>
2019-09-29 16:02:19 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -4,12 +4,12 @@ def main():
     mytransforms = [
         #transforms.Compose([transforms.ToTensor(), NORMALIZE]),
         #transforms.Compose([transforms.RandomHorizontalFlip(),transforms.ToTensor(), NORMALIZE]),
-        #transforms.Compose([transforms.RandomCrop(3,2),transforms.ToTensor(), NORMALIZE]),
-        transforms.Compose([transforms.Pad(4),transforms.ToTensor(), NORMALIZE]),
+        transforms.Compose([transforms.RandomCrop(3,2),transforms.ToTensor(), NORMALIZE]),
+        #transforms.Compose([transforms.Pad(4),transforms.ToTensor(), NORMALIZE]),
         transforms.Compose([transforms.RandomHorizontalFlip(),transforms.RandomCrop(3,2),transforms.ToTensor(), NORMALIZE]),
-        transforms.Compose([transforms.RandomHorizontalFlip(),transforms.Pad(4),transforms.ToTensor(), NORMALIZE]),
-        transforms.Compose([transforms.RandomCrop(3,2),transforms.Pad(4),transforms.ToTensor(), NORMALIZE]),
-        transforms.Compose([transforms.RandomHorizontalFlip(),transforms.RandomCrop(3,2),transforms.Pad(4),transforms.ToTensor(), NORMALIZE])
+        #transforms.Compose([transforms.RandomHorizontalFlip(),transforms.Pad(4),transforms.ToTensor(), NORMALIZE]),
+        #transforms.Compose([transforms.RandomCrop(3,2),transforms.Pad(4),transforms.ToTensor(), NORMALIZE]),
+        #transforms.Compose([transforms.RandomHorizontalFlip(),transforms.RandomCrop(3,2),transforms.Pad(4),transforms.ToTensor(), NORMALIZE])
     ]
     for i,transform in enumerate(mytransforms):
         run(transform, i)
