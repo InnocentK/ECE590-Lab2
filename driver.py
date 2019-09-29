460d3124d271cb42c7cd9fb298cc36206a61b651
Wambugu "Innocent" Kironji <innocent.kironji@duke.edu>
2019-09-29 18:57:11 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -5,14 +5,14 @@ def main():
     REG = 1e-4
     mytransform = transforms.Compose([transforms.ToTensor(), NORMALIZE])
     run(mytransform, 20)
-    run(mytransform, 23, MOMENTUM, 1e-3)
-    run(mytransform, 24, MOMENTUM, 1e-5)
-    run(mytransform, 21, 0.85)
-    run(mytransform, 21, 0.85, 1e-3)
-    run(mytransform, 21, 0.85, 1e-5)
-    run(mytransform, 22, 0.95)
-    run(mytransform, 21, 0.95, 1e-3)
-    run(mytransform, 21, 0.95, 1e-5)
+    run(mytransform, 21, MOMENTUM, 1e-3)
+    run(mytransform, 22, MOMENTUM, 1e-5)
+    run(mytransform, 23, 0.85)
+    run(mytransform, 24, 0.85, 1e-3)
+    run(mytransform, 25, 0.85, 1e-5)
+    run(mytransform, 26, 0.95)
+    run(mytransform, 27, 0.95, 1e-3)
+    run(mytransform, 28, 0.95, 1e-5)
 
     #mytransforms = [
         #transforms.Compose([transforms.ToTensor(), NORMALIZE])#,
