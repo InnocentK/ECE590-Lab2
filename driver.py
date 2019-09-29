9771c0e670b7b31a070c53c4ba0685645de654e5
Wambugu "Innocent" Kironji <innocent.kironji@duke.edu>
2019-09-29 00:59:39 -0400

    Changes for Driver

--- driver.py
+++ driver.py
@@ -1,4 +1,4 @@
-from lenet5-cifar10 import *
+from lenet5cifar10 import *
 
 def main():
     mytransforms = [
@@ -12,5 +12,5 @@ def main():
         transforms.Compose([transforms.RandomHorizontalFlip(),transforms.RandomCrop(2),transforms.Pad(2),transforms.ToTensor(), NORMALIZE])
     ]
     for i,transform in enumerate(mytransforms):
-        runCNN(transform, i)
+        run(transform, i)
 main()
