d29c1acd0472b461e02ddd13a1596da1e86a4caf
Wambugu "Innocent" Kironji <innocent.kironji@duke.edu>
2019-09-29 21:01:58 -0400

    Checking Higher Decay

--- driver.py
+++ driver.py
@@ -7,10 +7,14 @@ def main():
     run(mytransform, 300)
     #run(mytransform, 31, 0.5)
     #run(mytransform, 32, 0.25)
-    run(mytransform, 33, 0.1)
-    run(mytransform, 34, 0.05)
-    run(mytransform, 35, 0.02)
-    run(mytransform, 36, 0.01)
+    #run(mytransform, 33, 0.1)
+    #run(mytransform, 34, 0.05)
+    #run(mytransform, 35, 0.02)
+    #run(mytransform, 36, 0.01)
+    run(mytransform, 37, 1.10)
+    run(mytransform, 38, 1.25)
+    run(mytransform, 39, 1.50)
+    run(mytransform, 40, 2.00)
 
     #mytransforms = [
         #transforms.Compose([transforms.ToTensor(), NORMALIZE])#,
