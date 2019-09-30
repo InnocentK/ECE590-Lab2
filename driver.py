a6e7d11e6a719cb186a6220ed1c3bdf2ceda3c5c
Wambugu "Innocent" Kironji <innocent.kironji@duke.edu>
2019-09-29 20:36:29 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -4,9 +4,9 @@ def main():
     DECAY = 1.00
     DECAY_EPOCHS = 2
     mytransform = transforms.Compose([transforms.ToTensor(), NORMALIZE])
-    run(mytransform, 30)
-    run(mytransform, 31, 0.5)
-    run(mytransform, 32, 0.25)
+    run(mytransform, 300)
+    #run(mytransform, 31, 0.5)
+    #run(mytransform, 32, 0.25)
     run(mytransform, 33, 0.1)
     run(mytransform, 34, 0.05)
     run(mytransform, 35, 0.02)
