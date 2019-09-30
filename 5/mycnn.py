584d2363d9b68d34c9996c19f1dba283a021e481
Wambugu "Innocent" Kironji <innocent.kironji@duke.edu>
2019-09-30 14:32:20 -0400

    Update mycnn.py

--- 5/mycnn.py
+++ 5/mycnn.py
@@ -51,10 +51,10 @@ class InnocentNet(nn.Module):
     def forward(self, x):
         out = F.relu(self.conv1bn(self.conv1(x)))
         out = F.relu(self.conv12bn(self.conv12(out)))
-        out = F.max_pool2d(out, 2)
+        #out = F.max_pool2d(out, 2)
         out = F.relu(self.conv13bn(self.conv13(out)))
         out = F.relu(self.conv14bn(self.conv14(out)))
-        out = F.max_pool2d(out, 2)
+        #out = F.max_pool2d(out, 2)
         out = F.relu(self.conv15bn(self.conv15(out)))
         #out = F.relu(self.conv12(out))
         out = F.max_pool2d(out, 2)
