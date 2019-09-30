62cf9e746bb47e49578ba6be2c00057ea765ba41
Wambugu "Innocent" Kironji <innocent.kironji@duke.edu>
2019-09-30 17:30:35 -0400

    Update driver.py

--- 5/driver.py
+++ 5/driver.py
@@ -6,5 +6,6 @@ def main():
     #MOMENTUM = 0.85
     #mytransform = transforms.Compose([transforms.RandomHorizontalFlip(),transforms.ToTensor(), NORMALIZE])
     run(102)
+    run(103, epochs=65)
     run(103, epochs=100, loadTest=True)
 main()
